e21e6411ca2efb65b184cc9306352a674d16b74f
menuin <menuin@naver.com>
2021-02-04 16:02:16 +0900

    Getting the weather

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
-
+const weather=document.querySelector(".js-weather");
+const API_KEY ="3e04b18f88579f170d13a7a0c2381705";
 const COORDS ="coords";
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then (function(response){  // wait until data is fetched
+        return response.json();
+    }).then (function(json){ // wait until json is ready
+        const temperature=json.main.temp;
+        const place=json.name;
+        weather.innerText=`${temperature} @ ${place}`;
+    })
+
+} 
+
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -11,7 +25,8 @@ function handleGeoSuccess(position){
         latitude,
         longitude
     };
-    saveCoords(coordsObj)
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 function handleGeoError(){
     console.log("Can't access geo location");
@@ -25,7 +40,8 @@ function loadCoords(){
         askForCoords();
     }
     else{
-
+        const parsedCoords=JSON.parse(loadedCords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
     }
 }
 
